6dc1217603f7f9c4cdff9fd1eba3294905ce7316
Ehsan MohammadAbadi <ehsan.mohmmadi2130@gmail.com>
2025-12-15 22:19:23 +0330

    strict user access

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,8 +6,8 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    public function admin()
+    public function AdminDashboard()
     {
-        return view('admin_dashboard');
+        return view('admin.admin_dashboard');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AgentController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -31,4 +32,11 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
-Route::get('/admin/dashboard', [AdminController::class, 'admin'])->name('admin.dashboard');
+Route::middleware('auth','role:admin')->group(function () {
+    Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+
+});
+
+Route::middleware('auth','role:agent')->group(function () {
+    Route::get('/agent/dashboard', [AgentController::class, 'AgentDashboard'])->name('agent.dashboard');
+});
