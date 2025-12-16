e37794bf9ecdc2a48bb11ea0656b42467bb4261c
Ehsan MohammadAbadi <ehsan.mohmmadi2130@gmail.com>
2025-12-16 22:51:24 +0330

    Update LoginRequest.php

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\Auth;
 
+use App\Models\User;
 use Illuminate\Auth\Events\Lockout;
 use Illuminate\Contracts\Validation\Rule;
 use Illuminate\Foundation\Http\FormRequest;
@@ -41,6 +42,8 @@ class LoginRequest extends FormRequest
     public function authenticate(): void
     {
         $this->ensureIsNotRateLimited();
+
+        $user = User::where('email',$this->login)->orwhere('name',$this->login)->orwhere('phone',$this->login)->first();
 
         if (! Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))) {
             RateLimiter::hit($this->throttleKey());
